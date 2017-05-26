05915e7f6f5a783509f8c090b952596d1dd0695f
Yuki Yamashita <yyama0704@gmail.com>
2017-05-26 11:07:40 +0900

    add comment to answer

--- 03/answer/03_04.py
+++ 03/answer/03_04.py
@@ -1,5 +1,5 @@
 import math
-
+# listを使えばもっと良い実装ができるけれど，使わないとこんな感じかなぁ
 def is_goldbach(n):
     for i in range(2, n):
         if is_prime(i) and is_prime(n-i):
@@ -7,7 +7,7 @@ def is_goldbach(n):
             return True
     return False
 
-
+# これももっと早い実装がありそう？
 def is_prime(n):
     for i in range(2, int(math.sqrt(n))+1):
         if n % i == 0:
